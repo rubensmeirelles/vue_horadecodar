b84820c466c634c637f753a176a17e1d41ee60b1
rubensmeirelles <rrubens.meirelles@gmail.com>
2023-04-27 21:13:53 -0300

    ALTERAÇÃO INICIAL DO LAYOUT

---
+++ make_your_burguer/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="nav">
+        <router-link to="/">
+            <img src="" alt="logo" id="logo">
+        </router-link>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/pedidos">Pedidos</RouterLink>      
+    </div>
+</template>
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ make_your_burguer/src/views/Pedidos.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="pedidos">
+    <h1>Pedidos</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
