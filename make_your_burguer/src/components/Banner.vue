6312402dfdb397dc10e0cfad17805b39dd292d06
rubensmeirelles <rrubens.meirelles@gmail.com>
2023-04-27 22:20:40 -0300

    Adicionando o banner na página home

---
+++ make_your_burguer/src/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="main-banner">
+        <h1>Faça seu Hamburguer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .main-banner{
+        background-image: url('/img/burger.jpg');
+        background-position: 0 -350px;
+        background-size: cover;
+        height: 700px;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    .main-banner h1{
+        text-align: center;
+        color: #fff;
+        font-size: 60px;
+        padding: 20px 40px;
+    }
+</style>
--- make_your_burguer/src/views/Home.vue
+++ make_your_burguer/src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Monte o seu Burguer</h1>
+    <p>formulário</p>
   </div>
 </template>
 
-<script setup>
+<script>
+import Banner from '../components/Banner.vue'
+export default {
+  name: "Home",
+    components: {
+        Banner
+    }
+}
 
 </script>
 
